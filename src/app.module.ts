8f045c7209184b7f8586642f7351f65352e77072
nyko1 <yaokoua1@gmail.com>
2024-07-10 13:06:21 +0000

    Connection db ok et generate resource product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './resources/product/product.module';
 
 @Module({
-  imports: [],
+  // eslint-disable-next-line prettier/prettier
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+
+  const appURL = await app.getUrl();
+
+  Logger.log(`${process.env.APP_NAME} ${appURL} on ${process.env.APP_PORT}`);
 }
 bootstrap();
